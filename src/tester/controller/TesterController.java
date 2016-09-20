6e28188f593bd4a5071c296b911f6ae1956638a6
PayAsian <ptre8426@csddocs.org>
2016-09-20 13:06:06 -0600

    I've added some of the loops that i have figured out but i still need to finish the rest of them

--- src/tester/controller/TesterController.java
+++ src/tester/controller/TesterController.java
@@ -1,6 +1,73 @@
 package tester.controller;
 
 public class TesterController
-{
 
+{
+	public void start()
+	{
+	int x = 10;
+	
+	while ( x < 20) 
+	{
+	System.out.println("value of x : " + x );
+	x++;
+		System.out .println("\n");
+	}
+	
+	
+	}
+	
+	
+	{
+	if (true)
+	{
+		System.out.println("lul");
+	}
+		if (true)
+		{
+			System.out.println("i like food");
+		}
+			if (true)
+			{
+				System.out.println("ur bad kid git gud");
+			}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
 }
--- src/tester/controller/TesterRunner.java
+++ src/tester/controller/TesterRunner.java
@@ -1,6 +1,37 @@
 package tester.controller;
 
 public class TesterRunner
+
 {
+	public static void main (String [] args )
+	{	
+		TesterController testerController = new TesterController();
+		testerController.start();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
+
+	
